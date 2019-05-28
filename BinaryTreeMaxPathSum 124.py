9b4486129809985c32e9fbacd96a7cb50ae562e4
hightrol <494836651@qq.com>
2019-05-27 20:15:24 -0400

    Update BinaryTreeMaxPathSum 124.py

--- BinaryTreeMaxPathSum 124.py
+++ BinaryTreeMaxPathSum 124.py
@@ -5,22 +5,24 @@ Created on Sun May 26 22:40:08 2019
 @author: haimingwd
 """
 
-class Solution:
-    def setUp(self):
-        #self._maxDownPathSumDict = {None: 0}
-        self._maxSum = -10000000
-    
-    def helper(self, root):
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+        
+def maxPathSum(self, root: TreeNode) -> int:
+    maxSum = -10000000
+    def helper(root):
         if root:
-            leftSum, rightSum = self.helper(root.left), self.helper(root.right)
+            leftSum, rightSum = helper(root.left), helper(root.right)
             maxDownPathSum = root.val + max([leftSum, rightSum, 0])
             self._maxSum = max(self._maxSum, root.val + max(leftSum, 0) + max(rightSum, 0))
             return maxDownPathSum
         else:
-            return 0
-        
-    def maxPathSum(self, root: TreeNode) -> int:
-        self.setUp()
-        _ = self.helper(root)
-        return self._maxSum
+            return 0            
+    _ = helper(root)
+    return maxSum
+
+
     
