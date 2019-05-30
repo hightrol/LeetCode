4a86adff3821f51f8c5c37f81b19b8581fdc2826
hightrol <494836651@qq.com>
2019-05-29 22:37:11 -0400

    Create H-Index 274.py

---
+++ H-Index 274.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 22:35:54 2019
+
+@author: haimingwd
+"""
+
+def hIndex(citations):
+    if not citations:
+        return 0
+    n = len(citations)
+    l, r = 0, min(n, max(citations))
+    while r-l >= 1:
+        mid = (l+r+1)//2
+        if sum([ea >= mid for ea in citations]) >= mid:
+            l = mid
+        else:
+            r = mid-1
+    return l
