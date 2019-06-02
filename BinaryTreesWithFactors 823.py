72024d8aa95f67211ac87522ba08cb58d79d99cc
hightrol <494836651@qq.com>
2019-06-02 00:35:01 -0400

    Create BinaryTreesWithFactors 823.py

---
+++ BinaryTreesWithFactors 823.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  1 19:33:30 2019
+
+@author: haimingwd
+"""
+
+def numFactoredBinaryTrees(A):
+    A.sort()
+    headSum = {}
+    for i in A:
+        s = 1
+        for j in headSum:
+            if j > i ** 0.5:
+                break
+            if i % j == 0 and i//j in headSum:
+                mul = 1 if i // j == j else 2
+                s += mul * headSum[j] * headSum[i//j]                       
+        headSum[i] = s
+    return sum(headSum.values())
