adabc585a4c989637903bde3c0f70d2d68e455a3
hightrol <33535691+hightrol@users.noreply.github.com>
2019-12-18 23:47:06 -0500

    Create BinarySearchTreeIterator 173.py

---
+++ Cpp/BinarySearchTreeIterator 173.py
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class BSTIterator {
+    stack <TreeNode*> s;
+
+public:    
+    BSTIterator(TreeNode* root) {
+        if (root){
+            s.push(root);
+            while (s.top()->left){
+                s.push(s.top()->left);
+            }
+        }
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *r = s.top();
+        s.pop();
+        if(r->right){
+            s.push(r->right);
+            while (s.top()->left){
+                s.push(s.top()->left);
+            }
+        }
+        return r->val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !s.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
